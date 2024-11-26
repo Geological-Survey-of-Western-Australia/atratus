547e2c547c02e30c23a65b5321b1bb2328b08dbc
Jianli ZHI <Jianli.ZHI@dmirs.wa.gov.au>
2024-11-26 10:53:43 +0800

    Draft File Structure

---
+++ tests/unit/test_logging.py
@@ -0,0 +1,11 @@
+import unittest
+import json
+import sqlalchemy as sqla
+from pathlib import Path
+
+class TestLoggingFunctions(unittest.TestCase):
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
