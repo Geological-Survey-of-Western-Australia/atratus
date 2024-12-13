99e1c39fd77bf2bf23e04eccbe95216c3d695366
Luke SMITH <Luke.SMITH@demirs.wa.gov.au>
2024-12-13 17:23:10 +0800

    Add option to create from config

--- src/geo_digital_tools/database/create_v2.py
+++ src/geo_digital_tools/database/create_v2.py
@@ -1,26 +1,63 @@
-import typing
+import json
+from pathlib import Path
 
 import sqlalchemy as sqla
-from sqlalchemy import Column, Integer, MetaData, String, Table
 
-from geo_digital_tools.utils.exceptions import KnownException
-from geo_digital_tools.database import METADATA
+from geo_digital_tools.database import METADATA, SUPPORTED_TYPES, tables_from_config
+from geo_digital_tools.utils.exceptions import KnownException, exception_handler
 
 
 class CreateInterface:
-    """Helper class to create to a database"""
+    """Helper class to create a database"""
 
-    def __init__(self, engine: sqla.Engine | None = None):
-        # if engine is None:
-        KnownException("No engine was specified, using in-memory fall-back.")
-        self._fallback = True
-        self.engine = sqla.create_engine(
-            url="sqlite+pysqlite:///:memory:",
-            echo="debug",
-        )
-        # else:
-        #     self._fallback = False
-        #     self.engine = engine
+    def __init__(
+        self,
+        url: str | sqla.URL | None = None,
+        cfg_path: str | Path | None = None,
+    ):
+        self._fallback: bool = False
+        if cfg_path is not None:
+            self.from_config(cfg_path)
+        elif url is not None:
+            self.engine = sqla.create_engine(url=url)
+        else:
+            KnownException("No engine URL was specified, using in-memory fall-back.")
+            self._fallback = True
+            self.engine = sqla.create_engine(
+                url="sqlite+pysqlite:///:memory:",
+                echo="debug",
+            )
+
+    @exception_handler(should_raise=True)
+    def from_config(self, cfg_path: str | Path):
+        """Define an engine and database schema using config file
+
+        Template for config.json:
+        {
+            "sqlalchemy": {"sqlalchemy.url": "sqlite+pysqlite:///:memory:"},
+            "tables": {
+                "table1": {
+                    "col1": "String",
+                    "col2": "String"
+                },
+                "table2": {
+                    "col1": "String"
+                }
+            }
+        }
+        """
+        cfg_path = Path(cfg_path)
+        if not cfg_path.exists():
+            FileNotFoundError(f"{cfg_path.absolute()} not found.")
+
+        with open(cfg_path) as f:
+            db_config = json.load(f)
+
+        sqla_cfg = db_config.pop("sqlalchemy")
+
+        self.engine = sqla.engine_from_config(configuration=sqla_cfg)
+
+        tables_from_config(db_config)
 
     def define_db(self):
         """Overridable method in which a database is defined"""
@@ -28,73 +65,9 @@ class CreateInterface:
             "This method should be overwritten for your application"
         )
 
+    def validate(self):
+        pass
+
     def create_metadata(self):
         """Create objects in module level metadata store"""
         METADATA.create_all(self.engine)
-
-    def execute(self):
-        """Execute the engine with sqla"""
-        with self.engine.begin() as conn:
-            result = conn.execute()
-
-        return result
-
-
-if __name__ == "__main__":
-    # TODO Move this to atratus
-    class AtratusLocalInterface(CreateInterface):
-        """In-memory database to store Atratus extracts"""
-
-        def __init__(self, engine=None):
-            super().__init__(self)
-
-        @typing.override
-        def define_db(self):
-            r"""sql
-            CREATE TABLE IF NOT EXISTS files (
-                ANumber varchar(255),
-                file_path varchar(2000),
-                file_name varchar(255),
-                file_type varchar(255),
-                file_hash varchar(255),
-                file_year TEXT,
-                file_size int
-                );
-
-            CREATE TABLE IF NOT EXISTS reports (
-                ANumber varchar(255),
-                KeywordList varchar(2000),
-                TargetCommodityList varchar(2000),
-                centroid varchar(2000),
-                ReportDate TEXT,
-                CONFIDENTIALITY varchar(255)
-                );"""
-
-            files = Table(
-                "files",
-                METADATA,
-                Column("ANumber", String, primary_key=True),
-                Column("file_path", String),
-                Column("file_name", String),
-                Column("file_type", String),
-                Column("file_hash", String),
-                Column("file_year", String),
-                Column("file_size", Integer),
-            )
-
-            reports = Table(
-                "reports",
-                METADATA,
-                Column(
-                    "ANumber", String, primary_key=True
-                ),  # sqla.ForeignKey("files.ANumber")
-                Column("KeywordList", String),
-                Column("TargetCommodityList", String),
-                Column("centroid", String),
-                Column("ReportDate", String),
-                Column("CONFIDENTIALITY", String),
-            )
-
-    al = AtratusLocalInterface()
-    al.define_db()
-    al.create_metadata()
