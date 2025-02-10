49f0c7d57173a40228687ad2d8715543e65d81e9
Luke SMITH <Luke.SMITH@demirs.wa.gov.au>
2025-02-10 16:41:49 +0800

    Configure gdt standard logging
    use_gdt_logging now sets up nice default loggers.

--- src/geo_digital_tools/utils/gdt_logging.py
+++ src/geo_digital_tools/utils/gdt_logging.py
@@ -32,24 +32,50 @@ class KnownExceptionsFilter(logging.Filter):
     """
 
     def filter(self, lr: logging.LogRecord):
-        if (
-            lr.levelname == "ERROR"  # TODO: Use custom logging level?
-            or (lr.exc_info and type(lr.exc_info[1]).__name__ == "KnownException")
-        ):
+        if "KnownException" in lr.msg:
             # Capture exception and traceback
             lr_ = copy.copy(lr)
-            lr_.msg = f"Raised: {repr(lr_.exc_info[1])}, with message '{lr_.message}'"
-            lr_.exc_info = None  # (lr.exc_info[0], lr.exc_info[1], None)
-            lr_.exc_text = None  #
-
-            # Track count of this type of error?
-
+            if lr.exc_info is not None:
+                lr_.msg = f"Raised: {repr(lr_.exc_info[1])}, with message '{lr_.msg}'"
+                lr_.exc_info = None  # (lr.exc_info[0], lr.exc_info[1], None)
+                lr_.exc_text = None
             return lr_
         else:
             return False
 
 
-def use_gdt_logging(log_dir: str | Path = "logs", use_excepthook: bool = False):
+class NotKnownExceptionsFilter(logging.Filter):
+    """Filter to exclude KnownException logs from general log."""
+
+    def filter(self, lr: logging.LogRecord):
+        if "KnownException" not in lr.msg:
+            return True
+
+
+class KnownExceptionFormatter(logging.Formatter):
+    """Customise the format for KnownExceptions.
+
+    We want to make the KnownException log more useful, by including custom parameters,
+    such as filename, Step name, etc. Currently just the documented example from Python.
+    """
+
+    def formatException(self, exc_info):
+        """Format an exception so that it prints on a single line."""
+        result = super().formatException(exc_info)
+        return repr(result)  # or format into one line however you want to
+
+    def format(self, record):
+        s = super().format(record)
+        if record.exc_text:
+            s = s.replace("\n", "") + "|"
+        return s
+
+
+def use_gdt_logging(
+    name: str | None = None,
+    log_dir: str | Path = "logs",
+    use_excepthook: bool = False,
+):
     """Configure logging to gdt recommendation.
 
     Args:
@@ -67,39 +93,49 @@ def use_gdt_logging(log_dir: str | Path = "logs", use_excepthook: bool = False):
     log_dir = Path(log_dir)
     log_dir.mkdir(exist_ok=True, parents=True)
 
+    # Standard output logs of WARNING or above.
     standard_formatter = logging.Formatter(
-        "%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s  >>> %(message)s"
+        "%(asctime)s: %(name)s:%(lineno)s | %(levelname)s > %(message)s"
     )
 
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(logging.WARNING)
     stdout_handler.setFormatter(standard_formatter)
 
+    # Logfile that captures all program logs EXCEPT KnownException
     logfile_handler = logging.handlers.RotatingFileHandler(
         filename=log_dir / "gdt.log",
-        mode="a",
+        mode="w",
         maxBytes=1_048_576,
         backupCount=2,
     )
     logfile_handler.setLevel(logging.INFO)
     logfile_handler.setFormatter(standard_formatter)
+    logfile_handler.addFilter(NotKnownExceptionsFilter())
 
+    #  Logfile that captures only KnownException logs
     ke_handler = logging.handlers.RotatingFileHandler(
         filename=log_dir / "KnownExceptions.log",
-        mode="a",
+        mode="w",
         maxBytes=1_048_576,
         backupCount=2,
     )
-    ke_handler.setFormatter(standard_formatter)
+    ke_handler.setLevel(logging.INFO)
+    ke_handler.setFormatter(
+        logging.Formatter("%(asctime)s: %(name)s | %(levelname)s | > %(message)s")
+        # KnownExceptionFormatter("%(name)s: %(asctime)s | %(levelname)s | > %(message)s")
+    )
     ke_handler.addFilter(KnownExceptionsFilter())
 
+    # Initialise root logger.
     logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
     logger.addHandler(stdout_handler)
     logger.addHandler(logfile_handler)
     logger.addHandler(ke_handler)
 
     logger.warning(
-        f"Initialiased logger with gdt configuration. Writing to {log_dir.absolute()}."
+        f"Initialiased root logger with gdt configuration. Writing to {log_dir.absolute()}."
     )
 
     if use_excepthook:
